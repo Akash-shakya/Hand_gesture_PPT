3ece24d5a3a26c3eee6b229ea03a2cf54b849e47
Akash-Shakya <akashshakya821@gmail.com>
2024-03-28 00:48:31 +0530

    done upto 18:39

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from cvzone.HandTrackingModule import HandDetector
+#from cvzone.HandTrackingModule import HandDetector
 import cv2
 import os
-import numpy as np
+#import numpy as np
 
 # Parameters
 width, height = 1280, 720
@@ -12,3 +12,17 @@ folderPath = "D:\\Python_projects\\Hand_gesture_PPT\\ppt"
 cap = cv2.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
+
+# Get list of presentation images
+pathImages = sorted(os.listdir(folderPath), key=len)
+print(pathImages)
+
+while True:
+    # Get image frame
+    success, img = cap.read()
+    cv2.imshow("Image",img)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+
