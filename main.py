e14d97eaf5e20ba30234ce17fd5915941ae5f15e
Akash-Shakya <akashshakya821@gmail.com>
2024-03-30 17:11:01 +0530

    add pointer

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ cap.set(4, height)
 
 # Variables
 # imgList = []
-# delay = 30
-# buttonPressed = False
-# counter = 0
+delay = 30
+buttonPressed = False
+counter = 0
 # drawMode = False
 imgNumber = 0
 #hs, ws = int(120 *1), int(213 * 1)  # width and height of small image
@@ -43,14 +43,41 @@ while True:
     # Draw Gesture Threshold line
     cv2.line(img, (0, gestureThreshold), (width, gestureThreshold), (0, 255, 0), 10)
 
-    if hands:  # If hand is detected
+    if hands and buttonPressed is False: # If hand is detected
 
         hand = hands[0]
-        # cx, cy = hand["center"]
-        # lmList = hand["lmList"]  # List of 21 Landmark points
         fingers = detectorHand.fingersUp(hand)  # List of which fingers are up/
-        print(fingers)
+        cx, cy = hand["center"]
+        lmList = hand["lmList"]  # List of 21 Landmark points
+        indexFinger = lmList[8][0] , lmList[8][1]
 
+        cx, cy = hand["center"]
+        #print(fingers)
+
+        if cy <= gestureThreshold:  # If hand is at the height of the face
+            
+            #gestures 1 move left page
+            if fingers == [1, 0, 0, 0, 0]:
+                print("Left")
+                if imgNumber > 0:
+                    buttonPressed = True
+                    imgNumber -= 1
+
+            #gestures 2 move right page         
+            if fingers == [0, 0, 0, 0, 1]:
+                print("Right")
+                if imgNumber < len(pathImages) - 1:
+                    buttonPressed = True
+                    imgNumber += 1
+
+        #gesture 3 pointer
+        if fingers == [0, 1, 1, 0, 0]:
+            cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+    if buttonPressed:
+        counter += 1
+        if counter > delay:
+            counter = 0
+            buttonPressed = False
 
     # Define the new dimensions (width, height)
     new_width = 220
